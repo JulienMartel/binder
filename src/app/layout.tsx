2551abc024e9c2cc41a294493ad2be55d26ad4f6
ju <julienbmartel@gmail.com>
2023-02-21 18:37:06 -0600

    add: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,46 @@ import "./global.css";
 export const revalidate = 0;
 
 export const metadata = {
-  title: "Binder",
+  title: {
+    default: "Binder",
+    template: "%s | Binder",
+  },
+  description: "Find your next favorite book",
+  openGraph: {
+    title: "Binder",
+    description: "Find your next favorite book",
+    url: "https://binder.jubag.dev",
+    siteName: "Binder",
+    images: [
+      {
+        url: "https://binder.jubag.dev/og-sm.png",
+        width: 800,
+        height: 600,
+        alt: "Binder Logo",
+      },
+      {
+        url: "https://binder.jubag.dev/og-lg.png",
+        width: 1800,
+        height: 1600,
+        alt: "Binder Logo",
+      },
+    ],
+    locale: "en-CA",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Binder",
+    description: "Find your next favorite book",
+    creator: "@ju3ag",
+    images: ["https://binder.jubag.dev/og.png"],
+  },
+  appLinks: {
+    web: {
+      url: "https://binder.jubag.dev/app",
+      should_fallback: true,
+    },
+  },
 };
 
 import SupabaseListener from "../components/supabase-listener";
