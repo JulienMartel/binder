1000c8f0b5acf4c2a3a3389b305a63bc69c1ad3f
ju <julienbmartel@gmail.com>
2023-02-24 22:20:15 -0600

    fix: metadata

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -7,6 +7,49 @@ import Explainer from "./explainer";
 
 export const revalidate = 0;
 
+export const metadata = {
+  title: {
+    default: "Binder",
+    template: "%s | Binder",
+  },
+  description: "Find your next favorite book",
+  openGraph: {
+    title: "Binder",
+    description: "Find your next favorite book",
+    url: "https://binder.jubag.dev",
+    siteName: "Binder",
+    images: [
+      {
+        url: "https://binder.jubag.dev/og-sm.png",
+        width: 800,
+        height: 600,
+        alt: "Binder Logo",
+      },
+      {
+        url: "https://binder.jubag.dev/og-lg.png",
+        width: 1800,
+        height: 1600,
+        alt: "Binder Logo",
+      },
+    ],
+    locale: "en-CA",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Binder",
+    description: "Find your next favorite book",
+    creator: "@ju3ag",
+    images: ["https://binder.jubag.dev/og-twitter.png"],
+  },
+  appLinks: {
+    web: {
+      url: "https://binder.jubag.dev/app",
+      should_fallback: true,
+    },
+  },
+};
+
 export default async function Home() {
   return (
     <main>
