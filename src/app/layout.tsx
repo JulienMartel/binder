baa306f45a1dc31d2d3fcb1edc8716f49c53df08
ju <julienbmartel@gmail.com>
2023-02-24 22:10:05 -0600

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,49 +5,6 @@ import "./global.css";
 // do not cache this layout
 export const revalidate = 0;
 
-export const metadata = {
-  title: {
-    default: "Binder",
-    template: "%s | Binder",
-  },
-  description: "Find your next favorite book",
-  openGraph: {
-    title: "Binder",
-    description: "Find your next favorite book",
-    url: "https://binder.jubag.dev",
-    siteName: "Binder",
-    images: [
-      {
-        url: "https://binder.jubag.dev/og-sm.png",
-        width: 800,
-        height: 600,
-        alt: "Binder Logo",
-      },
-      {
-        url: "https://binder.jubag.dev/og-lg.png",
-        width: 1800,
-        height: 1600,
-        alt: "Binder Logo",
-      },
-    ],
-    locale: "en-CA",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Binder",
-    description: "Find your next favorite book",
-    creator: "@ju3ag",
-    images: ["https://binder.jubag.dev/og-lg.png"],
-  },
-  appLinks: {
-    web: {
-      url: "https://binder.jubag.dev/app",
-      should_fallback: true,
-    },
-  },
-};
-
 import SupabaseListener from "../components/supabase-listener";
 import SupabaseProvider from "@/components/supabase-provider";
 import { createServerClient } from "@/lib/supabase-server";
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,49 @@
 import { Provider } from "jotai";
 import { ThemeProvider } from "next-themes";
 
+export const metadata = {
+  title: {
+    default: "Binder",
+    template: "%s | Binder",
+  },
+  description: "Find your next favorite book",
+  openGraph: {
+    title: "Binder",
+    description: "Find your next favorite book",
+    url: "https://binder.jubag.dev",
+    siteName: "Binder",
+    images: [
+      {
+        url: "https://binder.jubag.dev/og-sm.png",
+        width: 800,
+        height: 600,
+        alt: "Binder Logo",
+      },
+      {
+        url: "https://binder.jubag.dev/og-lg.png",
+        width: 1800,
+        height: 1600,
+        alt: "Binder Logo",
+      },
+    ],
+    locale: "en-CA",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Binder",
+    description: "Find your next favorite book",
+    creator: "@ju3ag",
+    images: ["https://binder.jubag.dev/og-twitter.png"],
+  },
+  appLinks: {
+    web: {
+      url: "https://binder.jubag.dev/app",
+      should_fallback: true,
+    },
+  },
+};
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class">
